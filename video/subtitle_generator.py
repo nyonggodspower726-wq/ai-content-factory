5c387f0209f9bb1e3b98938e82b92ab6b0018974
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-27 22:04:18 +0100

    Update subtitle_generator.py

--- video/subtitle_generator.py
+++ video/subtitle_generator.py
@@ -1,1 +1,18 @@
+from moviepy.editor import *
 
+
+def create_subtitles(script):
+
+    print("Generating subtitles...")
+
+    subtitle = TextClip(
+        script,
+        fontsize=55,
+        color="white",
+        method="caption",
+        size=(900, None)
+    ).set_duration(30)
+
+    subtitle = subtitle.set_position(("center", "bottom"))
+
+    return subtitle
