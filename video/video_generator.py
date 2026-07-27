c11d97a0fcc0771c803887197bbe0d01ad6f2b5b
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-27 21:56:58 +0100

    Update video_generator.py

--- video/video_generator.py
+++ video/video_generator.py
@@ -1,1 +1,20 @@
+from openai import OpenAI
+from config import OPENAI_API_KEY
 
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+
+def generate_voice(script):
+
+    print("Generating AI voice...")
+
+    response = client.audio.speech.create(
+        model="gpt-4o-mini-tts",
+        voice="alloy",
+        input=script,
+    )
+
+    with open("output.mp3", "wb") as f:
+        f.write(response.read())
+
+    return "output.mp3"
