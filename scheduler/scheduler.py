95793c9a002e9c7005af61cbba3b16064e523d92
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-27 22:12:06 +0100

    Update scheduler.py

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,1 +1,19 @@
+import schedule
+import time
+from bot import main
 
+def run_bot():
+    print("Starting AI Content Factory...")
+    main()
+
+# First video
+schedule.every().day.at("09:00").do(run_bot)
+
+# Second video
+schedule.every().day.at("18:00").do(run_bot)
+
+print("Scheduler is running...")
+
+while True:
+    schedule.run_pending()
+    time.sleep(30)
