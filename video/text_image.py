a77709fa6475ef6a0e85af25cf110a473e8709b0
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-28 12:51:19 +0100

    Update text_image.py

--- video/text_image.py
+++ video/text_image.py
@@ -1,1 +1,26 @@
+from PIL import Image, ImageDraw, ImageFont
 
+def create_title_image(text):
+
+    width = 1080
+    height = 1920
+
+    image = Image.new("RGB", (width, height), (20, 20, 20))
+
+    draw = ImageDraw.Draw(image)
+
+    try:
+        font = ImageFont.truetype("DejaVuSans-Bold.ttf", 70)
+    except:
+        font = ImageFont.load_default()
+
+    draw.multiline_text(
+        (80, 700),
+        text,
+        fill="white",
+        font=font
+    )
+
+    image.save("title.png")
+
+    return "title.png"
