5d5ed0ad5c479ebe5e8be21e4306239689c095d2
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-28 09:18:30 +0100

    Update background_video.py

--- video/background_video.py
+++ video/background_video.py
@@ -1,1 +1,12 @@
+import requests
 
+def download_background():
+
+    url = "https://samplelib.com/lib/preview/mp4/sample-5s.mp4"
+
+    r = requests.get(url)
+
+    with open("background.mp4", "wb") as f:
+        f.write(r.content)
+
+    return "background.mp4"
