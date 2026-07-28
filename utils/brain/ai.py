dc106444ba0e0f1c262769df9c4eb32185e69d8b
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-28 13:41:18 +0100

    Update ai.py

--- utils/brain/ai.py
+++ utils/brain/ai.py
@@ -1,1 +1,21 @@
+from groq import Groq
+from config import GROQ_API_KEY
 
+client = Groq(api_key=GROQ_API_KEY)
+
+MODEL = "llama-3.3-70b-versatile"
+
+
+def ask_ai(prompt):
+
+    response = client.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
