7927d0e4ef768d3387a1d7255fd3dd4eb566c5d4
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-28 14:21:27 +0100

    Create planner.py

---
+++ brain/brain/brain/planner.py
@@ -0,0 +1,11 @@
+def daily_plan():
+
+    return [
+        "Find Trending Topic",
+        "Generate Script",
+        "Generate Caption",
+        "Generate Hashtags",
+        "Generate Voice",
+        "Create Video",
+        "Upload Video"
+    ]
