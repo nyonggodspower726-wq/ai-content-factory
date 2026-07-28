78c46050a989ed8f499bc0a3ad8cc787b9766b86
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-28 16:54:09 +0100

    Create planner.py

---
+++ planner.py
@@ -0,0 +1,18 @@
+def create_plan(goal):
+
+    """
+    Create a simple content/action plan.
+    """
+
+    plan = {
+        "goal": goal,
+        "steps": [
+            "Analyze topic",
+            "Generate content idea",
+            "Create script",
+            "Prepare media",
+            "Publish content"
+        ]
+    }
+
+    return plan
