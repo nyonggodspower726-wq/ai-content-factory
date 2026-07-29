dab46c1581edc8ff66f5e31a0259b7e4d73e0235
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-29 15:26:47 +0100

    Update video_generator.py

--- video/video_generator.py
+++ video/video_generator.py
@@ -57,3 +57,68 @@ def build_background_video(video_paths, duration):
     )
 
     return final_background
+# ==========================================
+# CREATE FINAL VIDEO
+# ==========================================
+
+def create_video(script, voice_file):
+
+    print("Creating professional AI video...")
+
+    keywords = [
+        word
+        for word in script.split()
+        if len(word) > 4
+    ]
+
+    search_term = " ".join(
+        keywords[:4]
+    )
+
+    video_urls = search_pexels_videos(
+        search_term
+    )
+
+    if len(video_urls) == 0:
+
+        print("No background videos found.")
+
+        return None
+
+    video_paths = download_videos(
+        video_urls
+    )
+
+    try:
+
+        audio = AudioFileClip(
+            voice_file
+        )
+
+        background = build_background_video(
+            video_paths,
+            audio.duration
+        )
+
+        if background is None:
+
+            print("Background creation failed.")
+
+            return None
+
+        final = background.set_audio(
+            audio
+        )
+
+        music_file = "assets/music/background.mp3"
+
+        final = add_background_music(
+            final,
+            music_file
+        )
+
+        # ==========================================
+        # EXPORT VIDEO
+        # ==========================================
+
+        output = "output/final_video.mp4"
