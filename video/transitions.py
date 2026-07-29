01d2eb8f2df432008e980546d76dfa37795bc2a6
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-29 14:21:26 +0100

    Update transitions.py

--- video/transitions.py
+++ video/transitions.py
@@ -1,1 +1,11 @@
+from moviepy.video.fx import all as vfx
 
+
+def apply_zoom(clip):
+
+    print("Applying zoom effect...")
+
+    return clip.fx(
+        vfx.resize,
+        lambda t: 1 + (0.05 * t / clip.duration)
+    )
