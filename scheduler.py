e4d3761a2dbbd3545f1ffb7e443896be83bf21f8
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-29 08:10:06 +0100

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -6,40 +6,56 @@ from bot import main
 
 POSTING_TIMES = [
     "08:00",
-    
 ]
 
 
 def start_scheduler():
 
+    print("=" * 50)
+    print("AI CONTENT FACTORY v1.0")
+    print("=" * 50)
     print("AI CONTENT FACTORY SCHEDULER STARTED")
 
-    completed = []
+    completed = set()
 
     while True:
 
-        nigeria_time = datetime.now(timezone(timedelta(hours=1)))
+        nigeria_time = datetime.now(
+            timezone(timedelta(hours=1))
+        )
+
         current_time = nigeria_time.strftime("%H:%M")
 
         print(f"Scheduler running | Nigeria time: {current_time}")
 
-        if current_time in POSTING_TIMES and current_time not in completed:
+        if (
+            current_time in POSTING_TIMES
+            and current_time not in completed
+        ):
 
+            print("=" * 50)
             print(f"Starting video generation: {current_time}")
+            print("=" * 50)
 
             try:
+
                 main()
+
                 print("Video task completed successfully.")
 
             except Exception as e:
+
                 print(f"Video task failed: {e}")
 
-            completed.append(current_time)
+            completed.add(current_time)
 
+        # Reset every midnight
         if current_time == "00:00":
+
             completed.clear()
 
-        time.sleep(60)
+        # Check every 10 seconds
+        time.sleep(10)
 
 
 if __name__ == "__main__":
