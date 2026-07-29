8832310ac7085dde507dca7c39b5323f62f3f32a
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-29 14:32:03 +0100

    Update background_music.py

--- video/background_music.py
+++ video/background_music.py
@@ -1,1 +1,29 @@
+from moviepy.editor import AudioFileClip, CompositeAudioClip
 
+
+def add_background_music(video, music_path, volume=0.15):
+
+    print("Adding background music...")
+
+    music = AudioFileClip(music_path)
+
+    music = music.volumex(volume)
+
+    music = music.set_duration(
+        video.duration
+    )
+
+    if video.audio:
+
+        audio = CompositeAudioClip(
+            [
+                video.audio,
+                music
+            ]
+        )
+
+    else:
+
+        audio = music
+
+    return video.set_audio(audio)
