d3e08f6489da61965b0c077261b9255e9023a022
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-29 11:03:21 +0100

    Update watermark.py

--- video/watermark.py
+++ video/watermark.py
@@ -1,1 +1,43 @@
+import subprocess
 
+
+def add_watermark(video_file):
+
+    print("Adding watermark...")
+
+    output = "output/watermarked_video.mp4"
+
+    command = [
+        "ffmpeg",
+        "-y",
+        "-i",
+        video_file,
+        "-vf",
+        "drawtext=text='AI CONTENT FACTORY':fontsize=35:fontcolor=white:x=w-text_w-30:y=h-text_h-30",
+        "-c:v",
+        "libx264",
+        "-preset",
+        "ultrafast",
+        "-threads",
+        "1",
+        "-c:a",
+        "copy",
+        output
+    ]
+
+    try:
+
+        subprocess.run(
+            command,
+            check=True
+        )
+
+        print("Watermark added successfully.")
+
+        return output
+
+    except Exception as e:
+
+        print(f"Watermark failed: {e}")
+
+        return video_file
