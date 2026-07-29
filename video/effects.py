e811c4cd3b2961cc747522554e35fa3a3da0e0da
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-29 03:17:32 +0100

    Update effects.py

--- video/effects.py
+++ video/effects.py
@@ -1,1 +1,26 @@
+from moviepy.editor import TextClip, CompositeVideoClip
 
+
+def add_hook(video, text):
+
+    print("Adding hook text...")
+
+    hook = TextClip(
+        text,
+        fontsize=70,
+        color="white",
+        method="caption",
+        size=(700, None)
+    )
+
+    hook = hook.set_duration(3)
+
+    hook = hook.set_position(
+        ("center", "top")
+    )
+
+    final = CompositeVideoClip(
+        [video, hook]
+    )
+
+    return final
