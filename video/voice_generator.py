008bc3b8e5fa57243c2e9a608c118dbf611191cf
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-29 16:02:28 +0100

    Update voice_generator.py

--- video/voice_generator.py
+++ video/voice_generator.py
@@ -9,12 +9,11 @@ VOICE = "en-US-GuyNeural"
 async def create_voice(text, output_file):
 
     communicate = edge_tts.Communicate(
-        text,
-        VOICE
+        text=text,
+        voice=VOICE
     )
 
     await communicate.save(output_file)
-
 
 
 def generate_voice(script):
@@ -26,17 +25,35 @@ def generate_voice(script):
     voice_file = "output/voice.mp3"
 
     try:
-        asyncio.run(
-            create_voice(
-                script,
-                voice_file
+
+        try:
+            asyncio.run(
+                create_voice(
+                    script,
+                    voice_file
+                )
             )
-        )
+
+        except RuntimeError:
+
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+
+            loop.run_until_complete(
+                create_voice(
+                    script,
+                    voice_file
+                )
+            )
+
+            loop.close()
 
         print("AI voice generated successfully.")
 
         return voice_file
 
     except Exception as e:
+
         print(f"Voice generation failed: {e}")
+
         return None
