3ba016e4ef62fea9709b69660c6e0cd9533f764b
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-29 11:40:02 +0100

    Update video_generator.py

--- video/video_generator.py
+++ video/video_generator.py
@@ -20,7 +20,7 @@ from moviepy.video.fx import all as vfx
 from config import PEXELS_API_KEY
 from video.effects import add_hook
 from video.watermark import add_watermark
-
+from video.background_music import add_background_music
 # ==========================================
 # SEARCH MULTIPLE PEXELS VIDEOS
 # ==========================================
