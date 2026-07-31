955a61d3ebbf4eace2fa5de284966e71faa54ef6
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-31 11:39:05 +0100

    Update video_generator.py

--- video/video_generator.py
+++ video/video_generator.py
@@ -81,3 +81,104 @@ def search_pexels_videos(query):
         print(f"Pexels search failed: {e}")
 
     return videos
+# ==========================================
+# DOWNLOAD VIDEOS
+# ==========================================
+
+def download_videos(video_urls):
+
+    os.makedirs("output/clips", exist_ok=True)
+
+    downloaded = []
+
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    for i, url in enumerate(video_urls):
+
+        filename = f"output/clips/clip_{i}.mp4"
+
+        try:
+
+            response = requests.get(
+                url,
+                headers=headers,
+                stream=True,
+                timeout=60
+            )
+
+            response.raise_for_status()
+
+            print(f"Downloading clip {i+1}...")
+
+            with open(filename, "wb") as f:
+
+                for chunk in response.iter_content(
+                    chunk_size=1024 * 1024
+                ):
+
+                    if chunk:
+                        f.write(chunk)
+
+            clip = VideoFileClip(filename)
+
+            clip.get_frame(0)
+
+            clip.close()
+
+            downloaded.append(filename)
+
+            print(f"Downloaded valid clip {i+1}")
+
+        except Exception as e:
+
+            print(f"Removing corrupted clip: {filename}")
+            print(e)
+
+            if os.path.exists(filename):
+                os.remove(filename)
+
+    return downloaded
+
+
+# ==========================================
+# BACKGROUND MUSIC
+# ==========================================
+
+def add_background_music(video, music_file):
+
+    if not os.path.exists(music_file):
+
+        print("No background music found.")
+
+        return video
+
+    try:
+
+        voice = video.audio
+
+        music = AudioFileClip(music_file)
+
+        music = music.volumex(0.12)
+
+        music = music.set_duration(video.duration)
+
+        final_audio = CompositeAudioClip(
+            [
+                music,
+                voice
+            ]
+        )
+
+        video = video.set_audio(final_audio)
+
+        print("Background music added.")
+
+        return video
+
+    except Exception as e:
+
+        print(f"Background music failed: {e}")
+
+        return video
