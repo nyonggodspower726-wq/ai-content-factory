d4e334aa905c739612f6503873dd9f181db17760
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-08-01 00:16:04 +0100

    Update script_factory.py

--- factory/script_factory.py
+++ factory/script_factory.py
@@ -1,1 +1,21 @@
+from brain.script_engine import generate_script
 
+
+class ScriptFactory:
+
+    def __init__(self):
+        print("Script Factory Ready")
+
+    def create(self, topic):
+
+        print("Generating Script...")
+
+        script = generate_script(topic)
+
+        return {
+            "topic": topic,
+            "script": script
+        }
+
+
+script_factory = ScriptFactory()
