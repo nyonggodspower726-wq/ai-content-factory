72c0774b94f444bc0e18dc79f15ccc5e7d2b8c8c
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-08-02 17:23:20 +0100

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,3 +1,4 @@
+import schedule
 import time
 from datetime import datetime
 from zoneinfo import ZoneInfo
@@ -5,49 +6,65 @@ from zoneinfo import ZoneInfo
 from bot import main
 
 
+# TEST TIME - CHANGE THIS ONLY
+TEST_TIME = "17:27"
+
+
 def run_bot():
 
     print("=" * 60)
-    print("SCHEDULER TRIGGERED - STARTING BOT")
+    print("SCHEDULE TRIGGERED")
     print("=" * 60)
 
     try:
+
         main()
+
+        print("=" * 60)
         print("VIDEO TASK COMPLETED SUCCESSFULLY")
+        print("=" * 60)
 
     except Exception as e:
+
+        print("=" * 60)
         print(f"BOT ERROR: {e}")
+        print("=" * 60)
+
 
 
 def start_scheduler():
 
+    print("=" * 60)
     print("AI CONTENT FACTORY SCHEDULER STARTED")
+    print("=" * 60)
 
-    already_run = False
+
+    schedule.clear()
+
+
+    # TEST MODE
+    schedule.every().day.at(TEST_TIME).do(run_bot)
+
+
+    print(
+        f"Scheduled trigger time: Nigeria {TEST_TIME}"
+    )
+
 
     while True:
 
-        now = datetime.now(
+
+        nigeria_time = datetime.now(
             ZoneInfo("Africa/Lagos")
-        )
+        ).strftime("%H:%M:%S")
+
 
         print(
-            f"Scheduler running | Nigeria time: {now.strftime('%H:%M:%S')}"
+            f"Scheduler running | Nigeria time: {nigeria_time}"
         )
 
-        # TEST: change this time manually
-        if (
-            now.hour == 17
-            and now.minute == 08
-            and not already_run
-        ):
 
-            run_bot()
-            already_run = True
-
-
-        if now.minute != 34:
-            already_run = False
+        schedule.run_pending()
 
 
         time.sleep(10)
