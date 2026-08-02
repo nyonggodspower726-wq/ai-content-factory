959cdd2a5dccaef74bb1314dd6029ae1095c9e4c
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-08-02 22:41:19 +0100

    Create google_client.py

---
+++ ai/google_client.py
@@ -0,0 +1,19 @@
+import os
+import google.generativeai as genai
+
+
+genai.configure(
+    api_key=os.getenv("GOOGLE_API_KEY")
+)
+
+
+model = genai.GenerativeModel(
+    "gemini-2.5-flash"
+)
+
+
+def ask(prompt):
+
+    response = model.generate_content(prompt)
+
+    return response.text
