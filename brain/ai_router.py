04a8c5e1ebab2059f1208d6c4870900a56d69a86
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-08-03 00:03:52 +0100

    Update ai_router.py

--- brain/ai_router.py
+++ brain/ai_router.py
@@ -13,25 +13,43 @@ def ask_ai(prompt):
             print(f"USING {provider.upper()} AI")
             print("=" * 60)
 
+
             if provider == "groq":
 
                 from ai.groq_client import ask
                 return ask(prompt)
+
 
             elif provider == "google":
 
                 from ai.google_client import ask
                 return ask(prompt)
 
+
             elif provider == "nvidia":
 
                 from ai.nvidia_client import ask
                 return ask(prompt)
 
+
+            elif provider == "openrouter":
+
+                from ai.openrouter_client import ask
+                return ask(prompt)
+
+
+            elif provider == "huggingface":
+
+                from ai.huggingface_client import ask
+                return ask(prompt)
+
+
         except Exception as e:
 
             print(f"{provider} failed")
             print(e)
+
             continue
+
 
     raise Exception("All AI providers failed.")
