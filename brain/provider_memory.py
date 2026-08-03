c2cb556c823f42a026430c5f9d7c8df5d2928c09
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-08-03 10:42:54 +0100

    Update provider_memory.py

--- brain/provider_memory.py
+++ brain/provider_memory.py
@@ -1,1 +1,20 @@
+class ProviderMemory:
 
+    def __init__(self):
+
+        self.current_provider = None
+
+    def get(self):
+
+        return self.current_provider
+
+    def set(self, provider):
+
+        self.current_provider = provider
+
+    def clear(self):
+
+        self.current_provider = None
+
+
+provider_memory = ProviderMemory()
