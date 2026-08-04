b583db90cbb13e8bbb8bd81c53013691e03e5d37
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-08-04 02:35:45 +0100

    Update google_client.py

--- Brain/google_client.py
+++ Brain/google_client.py
@@ -1,14 +1,13 @@
 import os
 import google.generativeai as genai
 
-
 genai.configure(
     api_key=os.getenv("GOOGLE_API_KEY")
 )
 
-
-# Updated Gemini model
-model = "gemini-2.5-flash"
+# Create Gemini model
+model = genai.GenerativeModel(
+    "gemini-2.5-flash"
 )
 
 
