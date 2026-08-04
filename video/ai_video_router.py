9ab1fa398aee40fed7523d93b765c947249e3d79
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-08-04 11:22:32 +0100

    Update ai_video_router.py

--- video/ai_video_router.py
+++ video/ai_video_router.py
@@ -1,6 +1,5 @@
 from video.minimax_worker import generate_minimax_video
 from video.wan_worker import generate_wan_video
-from video.hunyuan_worker import generate_hunyuan_video
 from video.pexels_worker import generate_pexels_video
 from video.unsplash_worker import generate_unsplash_video
 
@@ -15,8 +14,6 @@ class AIVideoRouter:
             ("MiniMax H3", generate_minimax_video),
 
             ("WAN 2.2", generate_wan_video),
-
-            ("HunyuanVideo", generate_hunyuan_video),
 
             ("Pexels", generate_pexels_video),
 
@@ -58,13 +55,13 @@ class AIVideoRouter:
                     f"{name} FAILED"
                 )
 
-                print(e)
+                print(str(e))
 
 
 
-        print(
-            "ALL VIDEO PROVIDERS FAILED"
-        )
+        print("=" * 60)
+        print("ALL VIDEO PROVIDERS FAILED")
+        print("=" * 60)
 
         return None
 
