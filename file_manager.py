62d9052543f697e0c2a7af399f46cc307e68629d
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-08-04 08:21:11 +0100

    Update file_manager.py

--- file_manager.py
+++ file_manager.py
@@ -1,10 +1,29 @@
+import json
 import os
 
 
 def save_text(filename, content):
+    """
+    Save either text or dictionaries automatically.
+    """
+
+    folder = os.path.dirname(filename)
+
+    if folder:
+        os.makedirs(folder, exist_ok=True)
 
     with open(filename, "w", encoding="utf-8") as file:
-        file.write(content)
+
+        if isinstance(content, (dict, list)):
+            json.dump(
+                content,
+                file,
+                indent=4,
+                ensure_ascii=False
+            )
+
+        else:
+            file.write(str(content))
 
     print(f"Saved: {filename}")
 
