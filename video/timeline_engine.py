fe2cced80062fa705b209aba49a56a23d9b05ba2
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-08-05 21:08:11 +0100

    Update timeline_engine.py

--- video/timeline_engine.py
+++ video/timeline_engine.py
@@ -1,8 +1,3 @@
-import os
-
-from moviepy.editor import VideoFileClip
-
-
 class TimelineEngine:
 
     def __init__(self):
@@ -11,80 +6,42 @@ class TimelineEngine:
         print("TIMELINE ENGINE")
         print("=" * 60)
 
-
-    def build(self, clips):
+    def build(self, scenes):
 
         timeline = []
 
         current_time = 0
 
-
-        if not clips:
+        if not scenes:
 
             return timeline
 
+        for index, scene in enumerate(scenes):
 
-        for index, clip in enumerate(clips):
-
-            duration = clip.get(
+            duration = scene.get(
                 "duration",
                 5
             )
 
+            scene["scene_id"] = index + 1
 
-            path = clip.get(
-                "clip"
+            scene["start"] = current_time
+
+            scene["end"] = current_time + duration
+
+            # AI image path
+            scene["image"] = scene.get(
+                "image"
             )
-
-
-            # ---------------------------------
-            # Create MoviePy clip object
-            # ---------------------------------
-
-            if path and os.path.exists(path):
-
-                try:
-
-                    clip_object = VideoFileClip(
-                        path
-                    )
-
-                    clip["clip_object"] = clip_object
-
-
-                except Exception as e:
-
-                    print(
-                        f"Clip loading failed: {e}"
-                    )
-
-                    clip["clip_object"] = None
-
-
-            else:
-
-                clip["clip_object"] = None
-
-
-
-            clip["scene_id"] = index + 1
-
-            clip["start"] = current_time
-
-            clip["end"] = current_time + duration
-
 
             timeline.append(
-                clip
+                scene
             )
 
-
             current_time += duration
-
 
         print(
             f"Timeline built with {len(timeline)} scenes."
         )
-
 
         return timeline
